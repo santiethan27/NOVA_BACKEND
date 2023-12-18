6ffcf65c2c21aafa2c408e74a9820c2661380771
santiethan27 <106702996+santiethan27@users.noreply.github.com>
2023-12-18 15:11:43 -0500

    Base del proyecto

---
+++ src/main/java/com/example/FirtsTest/FirtsTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.FirtsTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class FirtsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirtsTestApplication.class, args);
+	}
+
+}
